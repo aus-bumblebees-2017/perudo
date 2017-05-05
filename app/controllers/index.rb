7b5e342764854f01e850b394b4e7d43a79825967
Sarah Betack <msbetack@gmail.com>
2017-05-05 17:07:00 -0500

    Add real ocean stats and style

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,10 @@
 get '/' do 
+  page = ParseHelper.parse_page
+  @today_wave_range = ParseHelper.today_wave_height(page)
+  @today_temp = ParseHelper.today_sea_temp(page)
+  waves_range = ParseHelper.wave_heights_by_hour(page)
+  squeezed_days = ParseHelper.squeeze(waves_range)
+  @split_squeezed_days = ParseHelper.split_by_hour(squeezed_days)
   erb :index
 end
 
